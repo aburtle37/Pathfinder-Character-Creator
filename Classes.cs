05a67ddcd91f71e76ca26e11cb0599ed0213fe5b
aburtle37 <andrew.burtle@gmail.com>
2016-09-28 11:52:08 -0400

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,25 @@
+﻿namespace ChartacterCreator
+{
+    internal class Classes
+    {
+
+        public string className;
+        public string classDes;
+        public int hitDie;
+        public int wealthDice;
+        public int wealthDiceNum;
+        public int wealthMul;
+
+        public Classes(string inputClassName, string inputClassDes, int inputHitDie, int inputWealthDice, int inputWealthDiceNum, int inputWealthMul)
+        {
+
+            className = inputClassName;
+            classDes = inputClassDes;
+            hitDie = inputHitDie;
+            wealthDice = inputWealthDice;
+            wealthDiceNum = inputWealthDiceNum;
+            wealthMul = inputWealthMul;
+
+        }
+    }
+}
